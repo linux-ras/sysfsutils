7b52caa1d64a0afe4404403c1791ae3bd7ef13cd
Chris White <chwhite@redhat.com>
2021-06-25 18:00:25 +0000

    lib: Fixed a memory leak in lib/sysfs_driver.c
    - sysfs_get_driver_devices() had a case where the function returned
      before the dev pointer was closed.
    
    Warned-by: covscan
    Signed-off-by: Chris White <chwhite@redhat.com>

--- lib/sysfs_device.c
+++ lib/sysfs_device.c
@@ -247,7 +247,7 @@ struct sysfs_device *sysfs_open_device_tree(const char *path)
 			if (new == NULL) {
 				dbg_printf("Error opening device tree at %s\n",
 						cur->path);
-			  sysfs_close_device(devlist);
+				sysfs_close_device(devlist);
 				sysfs_close_device_tree(rootdev);
 				return NULL;
 			}
